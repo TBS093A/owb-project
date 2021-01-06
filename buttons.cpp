9df6d27fe249331a020cab51f3b0beeb3305ba14
TBS093A <zukkamil.44@gmail.com>
2021-01-06 12:57:33 +0100

    add playing functionality

--- buttons.cpp
+++ buttons.cpp
@@ -2,11 +2,15 @@
 #include <windows.h>
 #include "buttons.h"
 
-
 using namespace std;
+
+bool play = false;
 
 void addFile(){
 	cout<<"addFile()"<<endl;
+
+	mciSendString("open \"C:\Users\Lenovo Y700\Desktop\08_DRAGON_AGE.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+	
 	MessageBeep(MB_OK);
 }
 
@@ -32,5 +36,16 @@ void btnNext(){;
 
 void btnPausePlay(){
 	MessageBeep(MB_ICONERROR);
-	cout<<"btn Pause/Play"<<endl;
+	
+	if (!play) {
+		cout << "file playing...." << endl;
+		time_t t1 = time(0);
+		mciSendString("play mp3 from 10000 to 30000 wait", NULL, 0, NULL);
+		time_t t2 = time(0);
+		cout << "Duration of the play :" << t2-t1 << " seconds." << endl;
+	} else {
+		mciSendString("close mp3", NULL, 0, NULL);
+	}
+	
+	cout << "btn Pause/Play" << endl;
 }
